2e6510ffaa2e0da74cf13dd7ccf256b3c6cb079d
Dirk <dirk@thedelta.io>
2020-05-12 15:12:42 +0200

    init sms service

---
+++ V1/Services/SmsService.cs
@@ -0,0 +1,15 @@
+﻿using CoviIDApiCore.V1.Interfaces.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace CoviIDApiCore.V1.Services
+{
+    public class SmsService : ISmsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SmsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
