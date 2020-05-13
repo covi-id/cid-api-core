2c0d8a9a090c5ecc476956bfa7852926eea8949c
Roleen20 <roleen@thedelta.io>
2020-05-13 15:47:38 +0200

    Adding in session database

---
+++ Models/Database/Session.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoviIDApiCore.Models.Database
+{
+    public class Session : BaseModel<Guid>
+    {
+        public virtual Wallet Wallet { get; set; }
+        public DateTime ExpireAt { get; set; }
+        public bool isUsed { get; set; }
+    }
+}
---
+++ V1/Interfaces/Repositories/ISessionRepository.cs
@@ -0,0 +1,9 @@
+﻿using CoviIDApiCore.Models.Database;
+using System;
+
+namespace CoviIDApiCore.V1.Interfaces.Repositories
+{
+    interface ISessionRepository : IBaseRepository<Session, Guid>
+    {
+    }
+}
