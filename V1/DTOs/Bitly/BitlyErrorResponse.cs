b9ed422aa9a3018bde98a09c25510c7c834aa54a
Dirk <dirk@thedelta.io>
2020-05-13 10:23:18 +0200

    error response

---
+++ V1/DTOs/Bitly/BitlyErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace CoviIDApiCore.V1.DTOs.Bitly
+{
+    public class BitlyErrorResponse
+    {
+        [JsonProperty("message")] public string Message { get; set; }
+        [JsonProperty("errors")] public Error[] Errors { get; set; }
+        [JsonProperty("resource")] public string Resource { get; set; }
+        [JsonProperty("description")] public string Description { get; set; }
+    }
+
+    public class Error
+    {
+        [JsonProperty("field")] public string Field { get; set; }
+        [JsonProperty("message")] public string Message { get; set; }
+        [JsonProperty("error_code")] public string ErrorCode { get; set; }
+    }
+}
