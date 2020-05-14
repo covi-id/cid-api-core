b5db1c165581233de4caa1ea5b9ecb52a1d28d4f
Roleen20 <roleen@thedelta.io>
2020-05-14 17:21:33 +0200

    Set the mobile property as encrypted

--- V1/DTOs/Organisation/MobileUpdateCountRequest.cs
+++ V1/DTOs/Organisation/MobileUpdateCountRequest.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using CoviIDApiCore.V1.Attributes;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 
 namespace CoviIDApiCore.V1.DTOs.Organisation
@@ -6,6 +7,7 @@ namespace CoviIDApiCore.V1.DTOs.Organisation
     public class MobileUpdateCountRequest
     {
         [StringLength(16, MinimumLength = 9, ErrorMessage = "Invalid mobile number")]
+        [Encrypted(true)]
         public string MobileNumber { get; set; }
         [JsonProperty("long")] 
         public decimal Longitude { get; set; }
