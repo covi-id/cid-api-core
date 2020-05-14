91f2fa69dd057ab32965351e82ffc22940a10761
Dirk <dirk@thedelta.io>
2020-05-14 11:34:03 +0200

    added response class

---
+++ V1/DTOs/Clickatell/ClickatellResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace CoviIDApiCore.V1.DTOs.Clickatell
+{
+    public class ClickatellResponse
+    {
+        [JsonProperty("balance")] public double Balance { get; set; }
+        [JsonProperty("currency")] public string Currency { get; set; }
+    }
+}
