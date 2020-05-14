87ec539f6e347e6e88007fb20c4ce3edd451eea5
Dirk <dirk@thedelta.io>
2020-05-14 11:39:09 +0200

    added broker method

--- V1/Brokers/ClickatellBroker.cs
+++ V1/Brokers/ClickatellBroker.cs
@@ -3,13 +3,15 @@ using System.Threading.Tasks;
 using CoviIDApiCore.Exceptions;
 using CoviIDApiCore.V1.Constants;
 using CoviIDApiCore.V1.Interfaces.Brokers;
+using Newtonsoft.Json;
 
 namespace CoviIDApiCore.V1.Brokers
 {
     public class ClickatellBroker : IClickatellBroker
     {
         private readonly HttpClient _httpClient;
-        private static readonly string _sendPartialRoot = UrlConstants.PartialRoutes[UrlConstants.Routes.ClickatellSend];
+        private static readonly string _sendPartialRoute = UrlConstants.PartialRoutes[UrlConstants.Routes.ClickatellSend];
+        private static readonly string _balancePartialRoute =UrlConstants.PartialRoutes[UrlConstants.Routes.ClickatellBalance];
 
         public ClickatellBroker(HttpClient httpClient)
         {
@@ -18,10 +20,20 @@ namespace CoviIDApiCore.V1.Brokers
 
         public async Task SendSms(object payload)
         {
-            var response = await _httpClient.PostAsJsonAsync(_sendPartialRoot, payload);
+            var response = await _httpClient.PostAsJsonAsync(_sendPartialRoute, payload);
 
             if (!response.IsSuccessStatusCode)
                 throw new ClickatellException(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<ClickatellBroker> GetBalance()
+        {
+            var response = await _httpClient.GetAsync(_balancePartialRoute);
+
+            if(!response.IsSuccessStatusCode)
+                throw new ClickatellException(await response.Content.ReadAsStringAsync());
+
+            return JsonConvert.DeserializeObject<ClickatellBroker>(await response.Content.ReadAsStringAsync());
         }
     }
 }
--- V1/Interfaces/Brokers/IClickatellBroker.cs
+++ V1/Interfaces/Brokers/IClickatellBroker.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
+using CoviIDApiCore.V1.Brokers;
 
 namespace CoviIDApiCore.V1.Interfaces.Brokers
 {
     public interface IClickatellBroker
     {
         Task SendSms(object payload);
+        Task<ClickatellBroker> GetBalance();
     }
 }
