947faca94a37645df9c649beb768006869d30b25
Dirk <dirk@thedelta.io>
2020-05-20 12:58:37 +0200

    missing semi colon

--- V1/Constants/Messages.cs
+++ V1/Constants/Messages.cs
@@ -24,7 +24,7 @@
         public static readonly string Org_NegBalance = $"User can not be scanned out. Balance can not be less than 0."; //TODO: Copy change
         public static readonly string Org_EmailEmpty = $"Organisation email is empty.";
         public static readonly string Org_UserNotScannedIn = $"User can not be scanned out if no scan in has been recorded.";
-        public static readonly string Org_UserScannedIn = $"User can not be scanned in. User is already scanned in."
+        public static readonly string Org_UserScannedIn = $"User can not be scanned in. User is already scanned in.";
         public static readonly string Org_UserScannedOut = $"User has already been scanned out.";
 
         public static readonly string Org_AllWalletsCheckedOut = $"All instances of wallet has been logged out.";
